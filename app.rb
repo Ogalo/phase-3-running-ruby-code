bb88d2815bc8c2a0879da47b1f23ebe4e4ed208c
Otieno Harrison Omondi <ogaloharrison@gmail.com>
2022-08-16 11:38:56 +0300

    commit tree b178f3d8dd74dd83fd606f5af7d6401bd3409846

--- app.rb
+++ app.rb
@@ -1,13 +1,16 @@
-puts 'Hello Harry!'
-puts 'Hello Harry!'
-puts 'Hello Harry!'
+puts 'Hello World!'
+puts 'Hello World!'
+puts 'Hello World!'
  
-print 'Hello Ogalo'
-print 'Hello Ogalo'
-print 'Hello Ogalo'
+print 'Hello Wolrd'
+print 'Hello Wolrd'
+print 'Hello Wolrd'
+
 
 puts [1, 2, 3]
 print [1, 2, 3]
 p [1, 2, 3]
 pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+
+print 'Pass this test, please.'
 

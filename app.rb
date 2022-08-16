2fe05a9803ed7df76059b214cf5ab807c63a46ab
Otieno Harrison Omondi <ogaloharrison@gmail.com>
2022-08-16 11:18:41 +0300

    Done

---
+++ app.rb
@@ -0,0 +1,13 @@
+puts 'Hello Harry!'
+puts 'Hello Harry!'
+puts 'Hello Harry!'
+ 
+print 'Hello Ogalo'
+print 'Hello Ogalo'
+print 'Hello Ogalo'
+
+puts [1, 2, 3]
+print [1, 2, 3]
+p [1, 2, 3]
+pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+
